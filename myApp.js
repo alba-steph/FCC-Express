d62ff43824f00c311f6708c07255ccb410e1e755
Stephanie Albanese <stephanie@Stephanies-iMac.local>
2021-10-07 17:06:54 -0400

    serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -15,4 +15,9 @@ app.get("/", myHandler2);
 
 app.use("/public", express.static(__dirname + "/public"));
 
+var myHandler3 = function (req, res) {
+  res.json({ message: "Hello json" });
+};
+app.get("/json", myHandler3);
+
 module.exports = app;
