f3d77cd454fba88a1e3c9a3790997c702de475a3
Stephanie Albanese <stephanie@Stephanies-iMac.local>
2021-10-07 16:26:28 -0400

    add Express route that sends a file

--- myApp.js
+++ myApp.js
@@ -3,9 +3,14 @@ var app = express();
 
 console.log("Hello World");
 
-var myHandler = function (req, res) {
-  res.send("Hello Express");
+// var myHandler = function (req, res) {
+//   res.send("Hello Express");
+// };
+// app.get("/", myHandler);
+
+var myHandler2 = function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 };
-app.get("/", myHandler);
+app.get("/", myHandler2);
 
 module.exports = app;
