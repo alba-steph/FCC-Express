368e42416236f4f05d4bbece738376db8df10e84
Stephanie Albanese <stephanie@Stephanies-iMac.local>
2021-10-07 17:44:34 -0400

    create .env and pass environment variable to app

--- myApp.js
+++ myApp.js
@@ -15,8 +15,11 @@ app.get("/", myHandler2);
 
 app.use("/public", express.static(__dirname + "/public"));
 
+var response = "Hello json";
+if (process.env.MESSAGE_STYLE === "uppercase")
+  response = response.toUpperCase();
 var myHandler3 = function (req, res) {
-  res.json({ message: "Hello json" });
+  res.json({ message: response });
 };
 app.get("/json", myHandler3);
 
