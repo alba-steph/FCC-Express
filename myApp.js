bd5ead6efa3cd6a2d7fda4943e71c896fe110357
Stephanie Albanese <stephanie@Stephanies-iMac.local>
2021-10-10 07:36:01 -0400

    mount a post handler at the path /name

--- myApp.js
+++ myApp.js
@@ -8,6 +8,9 @@ console.log("Hello World");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// due to deprecated bodyParser, sub express in same code
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 var myMiddleware = function (req, res, next) {
   console.log(req.method, req.path, "-", req.ip);
@@ -48,8 +51,13 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
-app.route("/name").get((req, res) => {
-  res.json({ name: req.query.first + " " + req.query.last });
-});
+app
+  .route("/name")
+  .get((req, res) => {
+    res.json({ name: req.query.first + " " + req.query.last });
+  })
+  .post((req, res) => {
+    res.json({ name: req.body.first + " " + req.body.last });
+  });
 
 module.exports = app;
